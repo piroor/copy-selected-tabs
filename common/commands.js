477efb793e358953c646bac91a75e8f71ab734eb
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2024-04-09 22:31:26 +0900

    Cleanup

--- common/commands.js
+++ common/commands.js
@@ -86,30 +86,12 @@ export async function copyToClipboard(tabs, format) {
   if (typeof navigator.clipboard.write == 'function') {
     log('trying to write data to clipboard via Clipboard API');
     try {
-      /* const dt = new DataTransfer();
-         dt.items.add(plainText, 'text/plain');
-         dt.items.add(richText, 'text/html'); */
-
-      const ci1 = new ClipboardItem({
+      const clipboardItem = new ClipboardItem({
         ['text/plain']: plainText,
         ['text/html']:  richText,
       });
-
-      /* navigator.clipboard
-        .write([ci1])
-        .then(() => {
-          notifyCopied(tabs.length, plainText);
-        })
-        .catch((error) => {
-          notifyFailed(error);
-        }); */
-
-      await navigator.clipboard.write([ci1]).then(() => {
-        notifyCopied(tabs.length, plainText);
-      });
-
-      console.log('Text has been copied to clipboard');
-
+      await navigator.clipboard.write([clipboardItem]);
+      notifyCopied(tabs.length, plainText);
       return;
     }
     catch(error) {
